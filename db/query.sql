c435dcac6ffce54b02d2cf897580b6c5e4a44014
sevrinbe <sevrin.eldred@gmail.com>
2022-09-05 16:15:52 -0500

    more progress, finishing each function

--- db/query.sql
+++ db/query.sql
@@ -1,4 +1,4 @@
-SELECT employee_id AS Employee_ID,
+/* SELECT employee_id AS Employee_ID,
     first_name AS First_Name,
     last_name AS Last_Name,
     roles.title as Title,
@@ -12,21 +12,43 @@ INNER JOIN roles
 ON A.role_id = roles.position_id
 INNER JOIN departments 
 ON roles.department_id = departments.id
-ORDER BY A.manager_id;
+ORDER BY A.manager_id; */
 
- 
+-- VIEW EMPLOYEES AND THEN SELECT THAT EMPLOYEE TO UPDATE THEIR ROLE --
 
-/* 
+/* SELECT employee_id AS Employee_ID,
+    CONCAT(first_name, " ", last_name) AS Name,
+    roles.title as Role
+FROM employees
+INNER JOIN roles
+ON role_id = roles.position_id; */
+
+/*
 UPDATE employees SET manager_id = NULL WHERE manager_id = ---VARIABLE FOR EMPLOYEE ID---;
-DELETE employees WHERE employee_id = ---VARIABLE FOR EMPLOYEE ID---;
-*/
+DELETE employees WHERE employee_id = ---VARIABLE FOR EMPLOYEE ID---; */
+/* 
+UPDATE employees SET role_id = ${selectedRole}
+WHERE employees.first_name = ${employee.split[0]}; */
 
-
-SELECT roles.title AS roles
+/* SELECT roles.title AS roles
 FROM roles; 
 
 SELECT  departments.name AS Departments,
         departments.id as 'Deparment ID'
-FROM departments; 
+FROM departments;  */
 
 /* INSERT INTO roles (position_id) */
+
+
+INSERT INTO roles (title, salary)
+    VALUES (${roleTitle}, ${roleSalary}, ${roleDepartmentId});
+
+
+SELECT * FROM roles;
+
+INSERT INTO roles (title, salary, department_id)
+    VALUES ("Marketing_manager", 140000, );
+
+SELECT * FROM roles;
+
+SELECT * FROM roles;
--- index.js
+++ index.js
@@ -7,9 +7,14 @@ const inquirer = require("inquirer");
 const dbConfig = require("./config/dbConfig");
 const BaseEntity = require("./models/BaseEntity");
 const Department = require("./models/Department");
-
+const { up } = require('inquirer/lib/utils/readline');
 var dbConnection;
-
+let currentDepartments = [];
+let currentDepartmentsId = [];
+let currentRoles = [];
+let currentRolesId = [];
+let currentEmployees = [];
+let currentEmployeesId = [];
 
 async function main() {
     console.info(chalk.blue("=".repeat(30)));
@@ -30,7 +35,7 @@ function firstPrompt() {
       name: "task",
       message: "Would you like to do?",
       choices: [
-        "View All Deparments",
+        "View All Departments",
         "View All Roles",
         "View All Employees",
         "Add a Department",
@@ -42,8 +47,8 @@ function firstPrompt() {
     })
     .then(function ({ task }) {
       switch (task) {
-        case "View All Deparments":
-          viewAllDeparments();
+        case "View All Departments":
+          viewAllDepartments();
           break;
 
         case "View All Roles":
@@ -55,7 +60,7 @@ function firstPrompt() {
           break;
 
         case "Add a Department":
-          addADeparment();
+          addADepartment();
           break;
           
         case "Add Role":
@@ -63,7 +68,7 @@ function firstPrompt() {
           break;
           
         case "Add Employee":
-          addEmployees();
+          addEmployee();
           break;
           
         case "Update Employee Role":
@@ -71,7 +76,7 @@ function firstPrompt() {
           break;
 
         case "Remove Employees":
-          removeEmployees();
+          removeEmployee();
           break;
 
         case "End":
@@ -81,16 +86,112 @@ function firstPrompt() {
     });
 };
 
+async function getEmployees () {
+  let query = `SELECT CONCAT(first_name, ' ', last_name) as Name,
+              employees.employee_id AS employee_id
+              FROM employees;`;
+
+  await dbConnection.query(query).then((err) => {
+    try {
+      if (err) throw err;
+    } catch (listedEmployees) {
+      console.table(listedEmployees[0]);
+      for(i = 0; i < listedEmployees[0].length; i++) {
+        currentEmployees.push(listedEmployees[0][i].Name);
+        currentEmployeesId.push(listedEmployees[0][i].Department_ID);
+      }
+    }
+  }); 
+}
+
+async function viewRolesQuery() {
+  let query = `SELECT roles.title AS Roles,
+                      roles.position_id AS "Position ID"
+              FROM roles;`;
+  
+  await dbConnection.query(query).then((err, res) => {
+    try {
+      if (err) throw err;
+    } catch (res) {
+      console.log("\n");
+      console.table(res[0]);
+      for(i = 0; i < res[0].length; i++) {
+        currentRoles.push(res[0][i].Roles);
+        currentRolesId.push(res[0][i].ID);
+      }
+    }
+    console.log("All Roles Viewed!\n")
+});
+};
+
+
+function mostCurrentDepartments() {
+  let query = `
+  SELECT  departments.name AS Departments,
+  departments.id as Department_ID
+  FROM departments; `;
+  
+  dbConnection.query(query).then((err) => {
+    try {
+      if (err) throw err;
+    } catch (viewDepartments) {
+      console.table(viewDepartments[0]);
+      for(i = 0; i < viewDepartments[0].length; i++) {
+        currentDepartments.push(viewDepartments[0][i].Departments);
+        currentDepartmentsId.push(viewDepartments[0][i].Department_ID);
+
+      }
+    }
+  }); 
+};
+
+function viewAllDepartments () {
+  console.log("\n")
+  console.info(chalk.blue("=".repeat(30)));
+  console.log("Viewing Department(s)");
+  console.info(chalk.blue("=".repeat(30)));
+  console.log("\n")
+ 
+  let query = `
+  SELECT  departments.name AS Departments,
+  departments.id as 'Department ID'
+  FROM departments; `;
+  
+  dbConnection.query(query).then((err) => {
+    try {
+      if (err) throw err;
+    } catch (viewDepartments) {
+      console.table(viewDepartments[0]);
+    }
+    console.log("Departments Viewed!\n")
+    firstPrompt();
+  }); 
+};
+
+async function viewAllRoles () {
+  console.log("\n")
+  console.info(chalk.blue("=".repeat(30)));
+  console.log("Viewing all roles.");
+  console.info(chalk.blue("=".repeat(30)));
+  console.log("\n")
+  
+  await viewRolesQuery();
+  firstPrompt(); 
+};
 
 
 function viewEmployees() {
-  console.log("Viewing employees\n");
+  console.log("\n")
+  console.info(chalk.blue("=".repeat(30)));
+  console.log("Viewing employees");
+  console.info(chalk.blue("=".repeat(30)));
+  console.log("\n")
   
   let query =
     `SELECT employee_id AS "Employee ID",
       first_name AS "First Name",
       last_name AS "Last Name",
-      roles.title as Title,
+      roles.title as Role,
       roles.salary as Salary,
       departments.name as Department,
       (SELECT (SELECT CONCAT(first_name, ' ', last_name)) 
@@ -114,54 +215,239 @@ function viewEmployees() {
   });
 };
 
-function viewAllDeparments () {
-  console.log("Viewing employees by department...");
+function addADepartment() {
+  console.log("\n")
+  console.info(chalk.blue("=".repeat(30)));
+  console.log("Adding a Department");
+  console.info(chalk.blue("=".repeat(30)));
+  console.log("\n")
 
-  let query = `
-  SELECT  departments.name AS Departments,
-          departments.id as 'Deparment ID'
-  FROM departments; `;
+  let query = `SELECT departments.name AS Departments,
+  departments.id as 'Deparment ID'
+  FROM departments;`;
 
   dbConnection.query(query).then((err) => {
     try {
       if (err) throw err;
-    } catch (viewingEmployeesByDepartment) {
-      console.table(viewingEmployeesByDepartment[0]);
+    } catch (departments) {
+      console.log("\n")
+      console.table(departments[0]);
+      inquirer
+      .prompt({
+        type: "input",
+        message: "What is the name of the department you would like to add?",
+        name: "nameOfDepartment",
+      })
+      .then(function ({nameOfDepartment}) {
+        console.log(nameOfDepartment)
+        query = `INSERT INTO departments (name)
+        VALUES ('${nameOfDepartment}');`;
+    
+        dbConnection.query(query).then((err) => {
+          try {
+            if (err) throw err;
+          } catch (departments) {
+            viewAllDepartments();
+          }
+        })
+    })
     }
-    console.log("Departments Viewed!\n")
-    firstPrompt();
-  }); 
+  });
+};
+
+function addRole() {
+  console.info(chalk.blue("=".repeat(30)));
+  mostCurrentDepartments();
+  let query = `SELECT roles.title AS Roles
+  FROM roles;`;
+  
+  dbConnection.query(query).then((err) => {
+    try {
+      if (err) throw err;
+    } catch (roles) {
+      console.table(roles[0]);
+      inquirer
+      .prompt([{
+        type: "input",
+        message: "What role would you like to add?",
+        name: "roleTitle",
+      },
+      {
+        type: "input",
+        message: "What salary would you like for this role?",
+        name: "roleSalary",
+      },
+      {
+        type: "list",
+        message: "What department is this role in?",
+        name: "roleDepartmentId",
+        choices: currentDepartments,
+      }])
+      .then(function ({roleTitle, roleSalary, roleDepartmentId}) {
+        console.log(roleTitle, roleSalary, roleDepartmentId);
+
+        query = `INSERT INTO roles (title,salary,department_id) VALUES ("${roleTitle}","${roleSalary}","${currentDepartmentsId[currentDepartments.indexOf(roleDepartmentId)]}");`;
+
+        dbConnection.query(query).then((err) => {
+          try {
+            if(err) throw err;
+          } catch (newListOfRoles) {
+            viewAllRoles();
+          }
+        })
+      })
+    }
+  })
+
+
+
+}
+
+async function addEmployee() {
+  console.log("\n")
+  console.info(chalk.blue("=".repeat(30)));
+  console.log("Adding an employee.");
+  console.info(chalk.blue("=".repeat(30)));
+  console.log("\n")
+
+  await viewRolesQuery();
+  getEmployees();
+
+  let query = `SELECT * FROM employees;`
+
+  dbConnection.query(query).then((err) => {
+    try {
+
+    }
+  })
+  
+
+}
+
+function updateEmployeeRole() {
+  console.log("\n")
+  console.info(chalk.blue("=".repeat(30)));
+  console.log("Updating an employee's role.");
+  console.info(chalk.blue("=".repeat(30)));
+  console.log("\n")
+
+  let query = `SELECT employee_id AS "Employee_ID",
+  CONCAT(first_name, " ", last_name) AS Name,
+  roles.title as Role
+FROM employees
+INNER JOIN roles
+ON role_id = roles.position_id;`;
+
+let listedEmployees = [];
+
+dbConnection.query(query).then((err) => {
+  try {
+    if (err) throw err;
+  } catch (selectEmployeeFromList) {
+    console.table(selectEmployeeFromList[0]);
+    
+    
+
+    for(i = 0; i < selectEmployeeFromList[0].length; i++) {
+      listedEmployees.push(selectEmployeeFromList[0][i].Name);
+    }
+    
+    listedEmployees.push("Cancel");
+
+    inquirer
+    .prompt({
+      type: "list",
+      name: "option",
+      message: "Select an employee you would like to update.",
+      choices: listedEmployees,
+    })
+    .then(function ({option}) {
+      switch (option) {
+        case "Cancel":
+          console.log("\n")
+          console.info(chalk.blue("=".repeat(30)));
+          console.log("\n Cancelled\n ");
+          console.info(chalk.blue("=".repeat(30)));
+          console.log("\n");
+          firstPrompt();
+          break;
+        default: 
+        for(i = 0; i < selectEmployeeFromList[0].length; i++) {
+          if(option == selectEmployeeFromList[0][i].Name) 
+          {
+            changeEmployeeRole(selectEmployeeFromList[0][i])}
+        };
+      }
+    })
+  }
+})
+};
+
+function changeEmployeeRole (employee) {
+  console.log("\n")
+  console.info(chalk.blue("=".repeat(30)));
+  console.log(`${employee.Name}'s current role is ${employee.Role}.\n\nWhat role would you like to assign ${employee.Name}?`);  console.info(chalk.blue("=".repeat(30)));
+  console.log("\n")
+
+  
+  let query = `SELECT roles.title AS Roles,
+                      roles.position_id AS role_id
+  FROM roles;`;
+  
+  let newArray = [];
+  
+  dbConnection.query(query).then((err) => {
+    try {
+      if (err) throw err;
+    } catch (res) {
+      newArray = [...res[0]];
+    }
+    let options = [];
+    
+    
+    for(i = 0; i < newArray.length; i++) {
+      options.push(newArray[i].Roles);
+    }
+    options.push("Cancel");
+    
+     inquirer
+    .prompt({
+      type: "list",
+      name: "selectedRole",
+      message: "Please select a role for the employee.",
+      choices: options,
+    })
+    .then(function ({selectedRole}) {
+      if (selectedRole == "Cancel") {
+        firstPrompt();
+      }
+
+      let roleIdFromSelectedRole = newArray[options.indexOf(selectedRole)].role_id;
+
+      let query = `UPDATE employees SET role_id = ${roleIdFromSelectedRole}
+      WHERE employees.employee_id = ${employee.Employee_ID};`
+
+      dbConnection.query(query).then((err) => {
+        try {
+          if(err) throw err;
+        } catch (updatedEmployee) {
+          viewEmployees();          
+        }
+      })
+    })
+}); 
+
 };
 
 
 
 
 
-
-
-
-/** GIVEN a command-line application that accepts user input
-WHEN I start the application
-THEN I am presented with the following options: 
-
-
-
-
-
-
-
-
-
-
-WHEN I choose to view all roles
-THEN I am presented with the job title, role id, the department that role belongs to, and the salary for that role
-WHEN I choose to view all employees
-THEN I am presented with a formatted table showing employee data, including employee ids, first names, last names, job titles, departments, salaries, and managers that the employees report to
-WHEN I choose to add a department
-THEN I am prompted to enter the name of the department and that department is added to the database
-WHEN I choose to add a role
-THEN I am prompted to enter the name, salary, and department for the role and that role is added to the database
-WHEN I choose to add an employee
-THEN I am prompted to enter the employee’s first name, last name, role, and manager, and that employee is added to the database
-WHEN I choose to update an employee role
-THEN I am prompted to select an employee to update and their new role and this information is updated in the database */ 
+function removeEmployee() {
+  console.log("\n")
+  console.info(chalk.blue("=".repeat(30)));
+  console.log("Removing an employee.");
+  console.info(chalk.blue("=".repeat(30)));
+  console.log("\n")
+  
+}
